a0d13c0aba8fcfc345ed969ea43ae9c2b9fd0fff
vnagpal25 <2002vansh@gmail.com>
2026-04-30 22:32:01 -0400

    [CMake] Refactor PCMRIKernel module dependencies by removing unnecessary Eigen3 references; enhance EigenCompat.h to suppress deprecation warnings and ensure compatibility with Eigen types in LinearSolvers.

--- Modules/PCMRIKernel/ExtensionPoint/internal/EigenCompat.h
+++ Modules/PCMRIKernel/ExtensionPoint/internal/EigenCompat.h
@@ -9,4 +9,24 @@
 #endif
 #endif
 
+#ifndef EIGEN_NO_DEPRECATED_WARNING
+#define EIGEN_NO_DEPRECATED_WARNING
+#endif
+
+#ifndef EIGEN_DEPRECATED
+#define EIGEN_DEPRECATED
+#endif
+
+#ifndef EIGEN_DEPRECATED_WITH_REASON
+#define EIGEN_DEPRECATED_WITH_REASON(x)
+#endif
+
+#ifndef EIGEN_NOEXCEPT
+#define EIGEN_NOEXCEPT noexcept
+#endif
+
+#ifndef EIGEN_NOEXCEPT_IF
+#define EIGEN_NOEXCEPT_IF(x) noexcept(x)
+#endif
+
 #include <Eigen/Core>
