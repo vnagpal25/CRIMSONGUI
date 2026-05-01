18a5f4aa894bde680b29ea1855e533f7240640ee
vnagpal25 <2002vansh@gmail.com>
2026-05-01 13:26:04 -0400

    [CMake] Update Eigen3 configuration to prepend include directories for better compatibility; modify PCMRIKernel CMakeLists.txt to include Eigen3 before public targets, and clean up EigenCompat.h by removing deprecated warning definitions.

--- Modules/PCMRIKernel/ExtensionPoint/internal/EigenCompat.h
+++ Modules/PCMRIKernel/ExtensionPoint/internal/EigenCompat.h
@@ -13,10 +13,6 @@
 #define EIGEN_NO_DEPRECATED_WARNING
 #endif
 
-#ifndef EIGEN_DEPRECATED_WITH_REASON
-#define EIGEN_DEPRECATED_WITH_REASON(x)
-#endif
-
 #ifndef EIGEN_NOEXCEPT
 #define EIGEN_NOEXCEPT noexcept
 #endif
